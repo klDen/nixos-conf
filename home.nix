865b2ddbf0f85afe1e0b2ce8a508e78478d4d52c
Kenzyme L <kl@kenzymele.com>
2021-11-17 10:37:50 -0500

    Change electron apps to use native wayland

--- home.nix
+++ home.nix
@@ -16,7 +16,7 @@ rec {
     monoid
     curl tree git wget which htop file ntfs3g woeusb gnumake gcc binutils bc bind usbutils dmidecode
     ripgrep-all fd sd procs bandwhich lsof
-    signal-desktop # currently start-in-tray/icon broken on sway/wayland
+    signal-desktop
     vlc
     chromium # slow to build
     pcmanfm xournalpp zathura libsForQt5.ark
