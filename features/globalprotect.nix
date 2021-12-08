c26328369fc328444f5a63654d744f7f2231de61
Kenzyme L <kl@kenzymele.com>
2021-12-08 11:17:57 -0500

    add link to globalprotect not startable by installing package: enabling the service works

--- features/globalprotect.nix
+++ features/globalprotect.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 
 {
+  # https://github.com/yuezk/GlobalProtect-openconnect/issues/69#issuecomment-988957394
   #environment = {
   #  systemPackages = (with pkgs; [
   #    globalprotect-openconnect
@@ -10,6 +11,7 @@
   services.globalprotect = {
     enable = true;
     # if you need a Host Integrity Protection report
-    csdWrapper = "${pkgs.openconnect}/libexec/openconnect/hipreport.sh";
+    #csdWrapper = "${pkgs.openconnect}/libexec/openconnect/hipreport.sh";
   };
 }
+
