ba0a3a83167f75bda8cc44381b5ffbec7c6b9f1a
Kenzyme L <kl@kenzymele.com>
2022-04-12 13:05:48 -0400

    reactivate signal-desktop wayland

--- features/signal-desktop.nix
+++ features/signal-desktop.nix
@@ -1,17 +1,17 @@
 { lib, pkgs, ... }:
 let
-  #signal-desktop = pkgs.signal-desktop.overrideAttrs (old: {
-  #  installPhase = old.installPhase + ''
-  #    rm $out/bin/signal-desktop
+  signal-desktop = pkgs.signal-desktop.overrideAttrs (old: {
+    installPhase = old.installPhase + ''
+      rm $out/bin/signal-desktop
 
-  #    makeWrapper $out/lib/Signal/signal-desktop $out/bin/signal-desktop \
-  #      --prefix XDG_DATA_DIRS : $GSETTINGS_SCHEMAS_PATH \
-  #      --prefix PATH : ${lib.makeBinPath [pkgs.xdg-utils]} \
-  #      --add-flags "--ozone-platform=wayland --enable-features=UseOzonePlatform"
-  #  '';
-    # broken in wayland: --start-in-tray --enable-features=WebRTCPipeWireCapturer
-    # https://github.com/signalapp/Signal-Desktop/issues/5350
-  #});
+      makeWrapper $out/lib/Signal/signal-desktop $out/bin/signal-desktop \
+        --prefix XDG_DATA_DIRS : $GSETTINGS_SCHEMAS_PATH \
+        --prefix PATH : ${lib.makeBinPath [pkgs.xdg-utils]} \
+        --add-flags "--ozone-platform=wayland --enable-features=UseOzonePlatform --disable-gpu"
+    '';
+   # broken in wayland: --start-in-tray --enable-features=WebRTCPipeWireCapturer
+   # https://github.com/signalapp/Signal-Desktop/issues/5350
+  });
 in
 {
   environment = {
